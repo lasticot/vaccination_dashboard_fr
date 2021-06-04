ecfb56c3197b158c95401df33f0b8d91c3f7ae28
FranklinMaillot <24739376+lasticot@users.noreply.github.com>
2021-06-04 08:50:02 +0200

    cleaning

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
-from dashboard import compute_all, load_format_data, make_table
+from dashboard import  load_format_data, make_table
 
 clages_selected = {
     '20 - 29 ans': 29, 
